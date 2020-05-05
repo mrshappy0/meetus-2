b21bb863a29d68dc98c2088a6db06a9d5b2cf2b6
mrshappy0 <ajs11300@gmail.com>
2020-05-05 10:50:31 -0600

    Added starting files for the project

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,117 @@
 <script>
-	export let name;
+  import Header from "./UI/Header.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+  import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
+
+  let title = "";
+  let subtitle = "";
+  let address = "";
+  let email = "";
+  let description = "";
+  let imageUrl = "";
+
+  let meetups = [
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "Learn to code in 2 hours",
+      description:
+        "In this meetup, we will have some experts that teach you how to code!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "code@test.com",
+      isFavorite: false
+    },
+    {
+      id: "m2",
+      title: "Swim Together",
+      subtitle: "Let's go for some swimming",
+      description: "We will simply swim some rounds!",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
+      address: "27th Nerd Road, 32523 New York",
+      contactEmail: "swim@test.com",
+      isFavorite: false
+    }
+  ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      contactEmail: email,
+      address: address
+    };
+
+    // meetups.push(newMeetup); // DOES NOT WORK!
+    meetups = [newMeetup, ...meetups];
+  }
+
+  function toggleFavorite(event) {
+    const id = event.detail;
+    const updatedMeetup = { ...meetups.find(m => m.id === id) };
+    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+    const meetupIndex = meetups.findIndex(m => m.id === id);
+    const updatedMeetups = [...meetups];
+    updatedMeetups[meetupIndex] = updatedMeetup;
+    meetups = updatedMeetups;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    margin-top: 5rem;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
 </style>
+
+<Header />
+
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={event => (title = event.target.value)} />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={event => (subtitle = event.target.value)} />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      on:input={event => (address = event.target.value)} />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={event => (imageUrl = event.target.value)} />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      type="email"
+      value={email}
+      on:input={event => (email = event.target.value)} />
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      value={description}
+      on:input={event => (description = event.target.value)} />
+    <Button type="submit" caption="Save" />
+  </form>
+  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+</main>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import App from './App.svelte';
+import App from "./App.svelte";
+// import Header from './UI/Header.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  // target: document.querySelector('#app')
+  target: document.body,
 });
+
+// const header = new Header({
+// 	target: document.querySelector('#header')
+// })
 
 export default app;
